8424d210ddd45d6ac7d0ed73b0c6601f97aa4ac8
quang-ngh <quangngcs@gmail.com>
2023-02-24 17:08:15 +0700

    update grammar in .g4

--- assignment1/src/main/mt22/parser/.antlr/MT22Parser.py
+++ assignment1/src/main/mt22/parser/.antlr/MT22Parser.py
@@ -11,7 +11,7 @@ else:
 
 def serializedATN():
     with StringIO() as buf:
-        buf.write("\3\u608b\ua72a\u8133\ub9ed\u417c\u3be7\u7786\u5964\3M")
+        buf.write("\3\u608b\ua72a\u8133\ub9ed\u417c\u3be7\u7786\u5964\3\65")
         buf.write("\7\4\2\t\2\3\2\3\2\3\2\2\2\3\2\2\2\2\5\2\4\3\2\2\2\4\5")
         buf.write("\7\2\2\3\5\3\3\2\2\2\2")
         return buf.getvalue()
@@ -27,112 +27,83 @@ class MT22Parser ( Parser ):
 
     sharedContextCache = PredictionContextCache()
 
-    literalNames = [ "<INVALID>", "<INVALID>", "<INVALID>", "<INVALID>", 
-                     "<INVALID>", "<INVALID>", "<INVALID>", "<INVALID>", 
-                     "<INVALID>", "<INVALID>", "<INVALID>", "<INVALID>", 
-                     "<INVALID>", "<INVALID>", "<INVALID>", "<INVALID>", 
-                     "<INVALID>", "<INVALID>", "<INVALID>", "<INVALID>", 
-                     "<INVALID>", "<INVALID>", "<INVALID>", "<INVALID>", 
-                     "<INVALID>", "<INVALID>", "<INVALID>", "<INVALID>", 
-                     "'+'", "'-'", "'/'", "'*'", "'%'", "'!='", "'=='", 
-                     "'&&'", "'||'", "'!'", "'<'", "'<='", "'>'", "'>='", 
-                     "'::'", "'('", "')'", "'['", "']'", "'{'", "'}'", "'='" ]
+    literalNames = [ "<INVALID>", "<INVALID>", "<INVALID>", "'inherit'", 
+                     "'void'", "'return'", "'function'", "'true'", "'false'", 
+                     "'if'", "'else'", "'while'", "'for'", "'do'", "'break'", 
+                     "'continue'", "'integer'", "'float'", "'string'", "'boolean'", 
+                     "'array'", "'auto'", "'of'", "'+'", "'-'", "'/'", "'*'", 
+                     "'%'", "'!='", "'=='", "'&&'", "'||'", "'!'", "'<'", 
+                     "'<='", "'>'", "'>='", "'::'", "'('", "')'", "'['", 
+                     "']'", "'{'", "'}'", "'='" ]
 
-    symbolicNames = [ "<INVALID>", "WS", "ERROR_CHAR", "UNCLOSE_STRING", 
-                      "ILLEGAL_ESCAPE", "BLOCKCOMMENT", "INLINECOMMENT", 
-                      "IDENTIFIERS", "INHERIT", "VOID", "RETURN", "FUNCTION", 
-                      "TRUE", "FALSE", "IF", "ELSE", "WHILE", "FOR", "DO", 
-                      "BREAK", "CONTINUE", "INTEGER", "FLOAT", "STRING", 
-                      "BOOLEAN", "ARRAY", "AUTO", "OF", "ADD", "SUBSTRACT", 
-                      "DIVIDE", "MULTIPLY", "MODULO", "NOTEQUAL", "EQUAL", 
-                      "AND", "OR", "NOT", "LESS", "LEQ", "GREATER", "GEQ", 
-                      "STRING_CONCAT", "LEFT_PARENTHESIS", "RIGHT_PARENTHESIS", 
-                      "LEFT_SQUARE_BRACKET", "RIGHT_SQUARE_BRACKET", "LEFT_CURLY_BRACKET", 
-                      "RIGHT_CURLY_BRACKET", "ASSIGN", "A", "B", "C", "D", 
-                      "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", 
-                      "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", 
-                      "Y", "Z" ]
+    symbolicNames = [ "<INVALID>", "BLOCKCOMMENT", "INLINECOMMENT", "INHERIT", 
+                      "VOID", "RETURN", "FUNCTION", "TRUE", "FALSE", "IF", 
+                      "ELSE", "WHILE", "FOR", "DO", "BREAK", "CONTINUE", 
+                      "INTEGER", "FLOAT", "STRING", "BOOLEAN", "ARRAY", 
+                      "AUTO", "OF", "ADD", "SUBSTRACT", "DIVIDE", "MULTIPLY", 
+                      "MODULO", "NOTEQUAL", "EQUAL", "AND", "OR", "NOT", 
+                      "LESS", "LEQ", "GREATER", "GEQ", "STRING_CONCAT", 
+                      "LEFT_PARENTHESIS", "RIGHT_PARENTHESIS", "LEFT_SQUARE_BRACKET", 
+                      "RIGHT_SQUARE_BRACKET", "LEFT_CURLY_BRACKET", "RIGHT_CURLY_BRACKET", 
+                      "ASSIGN", "INTLIT", "BOOLIT", "IDENTIFIERS", "WS", 
+                      "ERROR_CHAR", "UNCLOSE_STRING", "ILLEGAL_ESCAPE" ]
 
     RULE_program = 0
 
     ruleNames =  [ "program" ]
 
     EOF = Token.EOF
-    WS=1
-    ERROR_CHAR=2
-    UNCLOSE_STRING=3
-    ILLEGAL_ESCAPE=4
-    BLOCKCOMMENT=5
-    INLINECOMMENT=6
-    IDENTIFIERS=7
-    INHERIT=8
-    VOID=9
-    RETURN=10
-    FUNCTION=11
-    TRUE=12
-    FALSE=13
-    IF=14
-    ELSE=15
-    WHILE=16
-    FOR=17
-    DO=18
-    BREAK=19
-    CONTINUE=20
-    INTEGER=21
-    FLOAT=22
-    STRING=23
-    BOOLEAN=24
-    ARRAY=25
-    AUTO=26
-    OF=27
-    ADD=28
-    SUBSTRACT=29
-    DIVIDE=30
-    MULTIPLY=31
-    MODULO=32
-    NOTEQUAL=33
-    EQUAL=34
-    AND=35
-    OR=36
-    NOT=37
-    LESS=38
-    LEQ=39
-    GREATER=40
-    GEQ=41
-    STRING_CONCAT=42
-    LEFT_PARENTHESIS=43
-    RIGHT_PARENTHESIS=44
-    LEFT_SQUARE_BRACKET=45
-    RIGHT_SQUARE_BRACKET=46
-    LEFT_CURLY_BRACKET=47
-    RIGHT_CURLY_BRACKET=48
-    ASSIGN=49
-    A=50
-    B=51
-    C=52
-    D=53
-    E=54
-    F=55
-    G=56
-    H=57
-    I=58
-    J=59
-    K=60
-    L=61
-    M=62
-    N=63
-    O=64
-    P=65
-    Q=66
-    R=67
-    S=68
-    T=69
-    U=70
-    V=71
-    W=72
-    X=73
-    Y=74
-    Z=75
+    BLOCKCOMMENT=1
+    INLINECOMMENT=2
+    INHERIT=3
+    VOID=4
+    RETURN=5
+    FUNCTION=6
+    TRUE=7
+    FALSE=8
+    IF=9
+    ELSE=10
+    WHILE=11
+    FOR=12
+    DO=13
+    BREAK=14
+    CONTINUE=15
+    INTEGER=16
+    FLOAT=17
+    STRING=18
+    BOOLEAN=19
+    ARRAY=20
+    AUTO=21
+    OF=22
+    ADD=23
+    SUBSTRACT=24
+    DIVIDE=25
+    MULTIPLY=26
+    MODULO=27
+    NOTEQUAL=28
+    EQUAL=29
+    AND=30
+    OR=31
+    NOT=32
+    LESS=33
+    LEQ=34
+    GREATER=35
+    GEQ=36
+    STRING_CONCAT=37
+    LEFT_PARENTHESIS=38
+    RIGHT_PARENTHESIS=39
+    LEFT_SQUARE_BRACKET=40
+    RIGHT_SQUARE_BRACKET=41
+    LEFT_CURLY_BRACKET=42
+    RIGHT_CURLY_BRACKET=43
+    ASSIGN=44
+    INTLIT=45
+    BOOLIT=46
+    IDENTIFIERS=47
+    WS=48
+    ERROR_CHAR=49
+    UNCLOSE_STRING=50
+    ILLEGAL_ESCAPE=51
 
     def __init__(self, input:TokenStream, output:TextIO = sys.stdout):
         super().__init__(input, output)
--- assignment1/src/main/mt22/parser/MT22.g4
+++ assignment1/src/main/mt22/parser/MT22.g4
@@ -15,144 +15,135 @@ options{
 	language=Python3;
 }
 
-program:  EOF ;
+program:  (vardecl | fundecl) + EOF;
 
+/*	=============================== Grammar - Parser - Declarations */
 
+// Variables declarations
+vardecl: list_of_ids COLON  primitive_type SEMI 
+		  | list_of_ids COLON primitive_type EQ expr SEMI;
+list_of_ids: IDENTIFIERS another_id_list | IDENTIFIERS;
+another_id_list: COMMA list_of_ids;
 
+funcdecl: 'funcdecl';
 
+expr: 'Expr';
+primitive_type: INTEGER | FLOAT | BOOLEAN | STRING | AUTO | VOID | arraytype;
+arraytype: ARRAY; 
 
-/*=========================	COMMENTS	============================== */
+//	Comments
 BLOCKCOMMENT: 
 				'/*' .*? '*/' -> skip;
 INLINECOMMENT: 
 				'//' ~[\r\n]* -> skip;
 
-/*=========================	VARIABLE DECLARE =========================*/
-
-
-/*=========================	FUNCTION DECLARE ========================= */
 
 /*	=============================== LEXER ================================== */
+//	KEYWORDS - Methods 
+INHERIT: 	'inherit';
+VOID: 		'void';
+RETURN:  	'return';
+FUNCTION: 	'function';
+
+//	Flow Statements
+IF: 		'if';
+ELSE: 		'else';
+
+
+//	Loop Statements
+WHILE: 		'while';
+FOR: 		'for';
+DO: 		'do';
+BREAK: 		'break';
+CONTINUE: 	'continue';
+
+//	Singular types
+INTEGER: 	'integer';
+FLOAT: 		'float';
+STRING: 	'string';
+BOOLEAN: 	'boolean';
+
+//	Compound types
+ARRAY: 		'array';
+AUTO: 		'auto';
+OF: 		'of';
+
+//	OPERATORS
+ADD: 						'+';
+SUBSTRACT: 					'-';
+DIVIDE:						'/';
+MULTIPLY:					'*';
+MODULO:						'%';
+
+NOTEQUAL:					'!=';
+EQUAL:						'==';
+AND:						'&&';
+OR:							'||';
+NOT:						'!';
+
+LESS:						'<';
+LEQ:						'<=';
+GREATER:					'>';
+GEQ:						'>=';
+STRING_CONCAT: 				'::';
+
+// SEPARATORS	
+LEFT_PARENTHESIS: 			'(';
+RIGHT_PARENTHESIS: 			')';
+LEFT_SQUARE_BRACKET: 		'[';
+RIGHT_SQUARE_BRACKET: 		']';
+LEFT_CURLY_BRACKET:			'{';
+RIGHT_CURLY_BRACKET:		'}';
+ASSIGN:						'=';
+COLON:						':';											// May not sure whether the colon is counted as one token in vardecl
+COMMA: 						',';											// May not sure whether the comma is counted as one token in vardecl
+SEMI: 						';';
+//	LITERALS
+INTLIT: [0] | ([1-9] (ZeroDigits | ('_'))*) {self.text = self.text.replace('_','')};
+FLOATLIT: INTLIT (DOT | ExpPart ('-'|'+')?) ZeroDigits* (DOT | ExpPart ('-'|'+'))? ZeroDigits*;
+STRINGLIT: DoubleQuote (StringChar*) DoubleQuote {
+	string = str(self.text)
+	self.text = string[1:-1]
+};
+BOOLIT: 'true' | 'false';
+
+
 //	IDENTIFIERS
 IDENTIFIERS: [_a-zA-Z][_a-zA-Z0-9]*;
 
 
-//	KEYWORDS - Methods 
-INHERIT: 'inherit';
-VOID: 'void';
-RETURN:  'return';
-FUNCTION: 'function';
 
-//	KEYWORDS - Values
-TRUE: 'true';
-FALSE: 'false';
+/*=========================	 Fragments =====================================*/
 
-//	Flow Statements
-IF: 'if';
-ELSE: 'else';
+fragment IntPart: INTLIT;
+fragment DOT: '.';
+fragment ExpPart: [eE];
+fragment ZeroDigits: [0-9];
+fragment EscapeSeqs: '\\'[tbfrn"\\];
+fragment Exp: [eE];
 
+fragment DoubleQuote: '"';
+fragment StringChar: ~[\t\b\n\r\f"\\]
+					| ESCSeq;
+fragment ESCSeq: '\\'[tbnrf"\\];
 
-//	Loop Statements
-WHILE: 'while';
-FOR: 'for';
-DO: 'do';
-BREAK: 'break';
-CONTINUE: 'continue';
-
-//	Singular types
-INTEGER: 'integer';
-FLOAT: 'float';
-STRING: 'string';
-BOOLEAN: 'boolean';
-
-//	Compound types
-ARRAY: 'array';
-AUTO: 'auto';
-OF: 'of';
-
-//	OPERATORS
-ADD: 				'+';
-SUBSTRACT: 			'-';
-DIVIDE:				'/';
-MULTIPLY:			'*';
-MODULO:				'%';
-
-NOTEQUAL:			'!=';
-EQUAL:				'==';
-AND:				'&&';
-OR:					'||';
-NOT:				'!';
-
-LESS:				'<';
-LEQ:				'<=';
-GREATER:			'>';
-GEQ:				'>=';
-STRING_CONCAT: 		'::';
-
-// SEPARATORS	
-LEFT_PARENTHESIS: 		'(';
-RIGHT_PARENTHESIS: 		')';
-LEFT_SQUARE_BRACKET: 	'[';
-RIGHT_SQUARE_BRACKET: 	']';
-LEFT_CURLY_BRACKET:		'{';
-RIGHT_CURLY_BRACKET:	'}';
-ASSIGN:					'=';
-
-
-//	LITERALS
-INTLIT: [0] | ([1-9] (ZeroDigits | ('_'))*) {self.text = self.text.replace('_','')};
-BOOLIT:
-		TRUE
-		| FALSE;
 
 //	Raise Error
-
 WS : [ \t\b\f\r\n]+ -> skip ; // skip spaces, tabs, backspace, form feed, carriage return, newline
 
 ERROR_CHAR: .{
 	raise ErrorToken(self.text)
 };
-UNCLOSE_STRING: .{
-	raise UncloseString(self.text)	
+UNCLOSE_STRING: DoubleQuote StringChar* ([\b\t\f\n\r"\\] | EOF)
+    {
+        unclose_str = str(self.text)
+        possible = ['\b', '\t', '\f', '\n', '\r', '"', '\\']
+        if unclose_str[-1] in possible:
+            raise UncloseString(unclose_str[1:-1])
+        else:
+            raise UncloseString(unclose_str[1:])
 };
+
 ILLEGAL_ESCAPE: .{
 	raise IllegalEscape(self.text)
 };
-
-/*=========================	 Fragments =====================================*/
-
-fragment ZeroDigits: [0-9];
-fragment DoubleQuote: '"';
-fragment EscapeSeqs: '\\'[tbfrn"\\];
-fragment StringChar: 
-					~[\t\b\f\r\n"\\]
-					| ( (~[DoubleQuote]) | ([DoubleQuote][DoubleQuote]))*
-					| EscapeSeqs;
-fragment Exp: [E];
-fragment Minus: '-';
-A:[a];
-B:[b];
-C:[c];
-D:[d];
-E:[e];
-F:[f];
-G:[g];
-H:[h];
-I:[i];
-J:[j];
-K:[k];
-L:[l];
-M:[m];
-N:[n];
-O:[o];
-P:[p];
-Q:[q];
-R:[r];
-S:[s];
-T:[t];
-U:[u];
-V:[v];
-W:[w];
-X:[x];
-Y:[y];
-Z:[z];
